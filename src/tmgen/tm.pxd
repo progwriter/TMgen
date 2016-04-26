0eaf658f442c7edf1be200ed572ea9a0e9259860
Victor Heorhiadi <victor@cs.unc.edu>
2016-04-26 14:07:16 -0400

    Implemented a TrafficMatrix class that has helper slicing methods

--- src/tmgen/tm.pxd
+++ src/tmgen/tm.pxd
@@ -1,5 +1,11 @@
 import numpy
 cimport numpy
 
+ctypedef enum entry_mode: ALL, MIN, MAX, MEAN
+
 cdef class TrafficMatrix:
     cdef public numpy.ndarray matrix
+    cpdef at_time(self, int t)
+    cpdef between(self, int o, int d, entry_mode mode)
+    cpdef to_pickle(self, fname)
+    cpdef worst_case(self)
--- src/tmgen/tm.pyx
+++ src/tmgen/tm.pyx
@@ -1,6 +1,36 @@
-import numpy
 cimport numpy
+import cPickle
+
+from tm cimport entry_mode
 
 cdef class TrafficMatrix:
-    def __init__(self):
-        pass
+    def __init__(self, numpy.ndarray tm):
+        self.matrix = tm
+
+    cpdef at_time(self, int t):
+        return self.matrix[:, :, t]
+
+    cpdef between(self, int o, int d, entry_mode mode):
+        if mode == ALL:
+            return self.matrix[o, d, :]
+        elif mode == MIN:
+            return numpy.min(self.matrix[o, d, :])
+        elif mode == MAX:
+            return numpy.max(self.matrix[o, d, :])
+        elif mode == MEAN:
+            return numpy.mean(self.matrix[o, d, :])
+
+    cpdef to_pickle(self, fname):
+        with open(fname, 'w') as f:
+            cPickle.dump(self.matrix, f)
+
+    cpdef worst_case(self):
+        return self.matrix.max(axis=2)
+
+    @staticmethod
+    def from_pickle(self, fname):
+        with open(fname, 'r') as f:
+            return TrafficMatrix(cPickle.load(f))
+
+    def __repr__(self):
+        return repr(self.matrix)
