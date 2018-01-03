882966e51664f8e397548cd7c795871f6191da46
Victor Heorhiadi <progwriter@users.noreply.github.com>
2018-01-03 14:16:35 -0800

    Simplifying generation of random gravity TMs, in attempt to fix #1 (#3)
    * Simplifying generation of random gravity TMs, in attempt to fix #1

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,14 @@
 # coding: utf-8
 
-# TODO: fixtures
-# TODO: non-negativity
-# TODO: correct shape
+from tmgen.models import modulated_gravity_tm
+
+
+def test_modulated_gravity():
+    tm = modulated_gravity_tm(num_nodes=2, num_tms=10, mean_traffic=1.0)
+
+    # test number of dimensions
+    assert tm.matrix.ndim == 3
+    assert tm.matrix.shape == (2, 2, 10)
+
+    # test non-negativity
+    assert tm.matrix.all() > 0
